d1215bb89d730eafbb335ef15f1a688d5e0cbb68
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-09-21 22:28:33 -0500

    corrected return line placement

--- public static string FizzBuzz(int x).cs
+++ public static string FizzBuzz(int x).cs
@@ -1,10 +1,10 @@
 public static string FizzBuzz(int x)
 {
 if(int x % 3 == 0 && x % 5 == 0)
-return "fizz";
-else if (int x % 5 ==0)
-return "buzz";
-else if (int x % 3 == 0)
 return "fizzbuzz";
+if (int x % 5 ==0)
+return "buzz";
+if (int x % 3 == 0)
+return "fizz";
 else return "No fizz, no buzz, and worst of all, no fizzbuzz";
 }
